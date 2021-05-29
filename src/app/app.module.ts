93e556980bc46d11138f9658d71b0dd4fb5240fd
oubeidk <kefioubeid@gmail.com>
2021-05-29 14:48:21 +0100

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,27 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCwBk-oZtFGPcabIn3U1YwR5FxONnjdFek",
+    authDomain: "to-dolist-b7007.firebaseapp.com",
+    projectId: "to-dolist-b7007",
+    storageBucket: "to-dolist-b7007.appspot.com",
+    messagingSenderId: "194247486195",
+    appId: "1:194247486195:web:79375f2113b70fc2262586",
+    measurementId: "G-KN1TF1MTKP"
+};
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,56 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  currentDate: string;
+  myTask: string;
+  addTask: boolean;
+  tasks = [];
+  today: number = Date.now();
+
+  constructor(public afDB: AngularFireDatabase,) {
+
+    const date = new Date();
+    //convertir la date en chaîne de caractère au format Français
+    const options = { weekday: 'long', month: 'long', day: 'numeric' };
+    this.currentDate = date.toLocaleDateString('fr-FR', options);
+    this.getTasks();
+  }
+  // ajouter un nouvel item à notre table Tasks
+  addTaskToFirebase() {
+    this.afDB.list('Tasks/').push({
+      text: this.myTask,
+      date: new Date().toISOString(),
+      checked: false
+    });
+    this.showForm();
+  }
+  //changer la valeur de variable 'addTask' en  booléenne
+  showForm() {
+    this.addTask = !this.addTask;
+    this.myTask = '';
+  }
+  //parcourir notre base firebase Tasks/ et récupérer les informations de nos tâches
+  getTasks() {
+    this.afDB.list('Tasks/').snapshotChanges(['child_added', 'child_removed']).subscribe(actions => {
+      this.tasks = [];
+      actions.forEach(action => {
+        this.tasks.push({
+          key: action.key,
+          text: action.payload.exportVal().text,
+          hour: action.payload.exportVal().date.substring(11, 16),
+          checked: action.payload.exportVal().checked
+        });
+      });
+    });
+  }
+  //permet d’afficher dans la console la valeur de checkbox (true ou false) et mettre à jour la valeur du champ checked de notre tâche Firebase.
+
+  changeCheckState(ev: any) {
+    console.log('checked: ' + ev.checked);
+    this.afDB.object('Tasks/' + ev.key + '/checked/').set(ev.checked);
+  }
+  deleteTask(task: any) {
+    this.afDB.list('Tasks/').remove(task.key);
+  }
 
 }
